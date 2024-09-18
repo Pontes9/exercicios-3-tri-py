b0c32b4a03e78aaf5cd1e4c1d36a1582d5d7a687
Pontes9 <162999567+Pontes9@users.noreply.github.com>
2024-09-18 11:33:17 -0300

    Delete atividade 19.js

--- atividade 19.js
+++
@@ -1,7 +0,0 @@
-function verificarIdadeParaVotar(idade) {
-    if (idade >= 18) {
-        console.log("A pessoa pode votar.");
-    } else {
-        console.log("A pessoa não pode votar.");
-    }
-}
