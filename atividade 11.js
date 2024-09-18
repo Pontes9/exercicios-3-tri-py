695b6a2b7ca88971487471ef6f464ed78fd2da20
Pontes9 <162999567+Pontes9@users.noreply.github.com>
2024-09-18 11:31:34 -0300

    Delete atividade 11.js

--- atividade 11.js
+++
@@ -1,9 +0,0 @@
-function verificarNumero(num) {
-    if (num > 0) {
-        console.log("O número é positivo.");
-    } else if (num < 0) {
-        console.log("O número é negativo.");
-    } else {
-        console.log("O número é zero.");
-    }
-}
