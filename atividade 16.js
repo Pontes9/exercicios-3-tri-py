f277413f5a886f1ed6b83590499571fbdd4ed6d0
Pontes9 <162999567+Pontes9@users.noreply.github.com>
2024-09-18 11:32:50 -0300

    Delete atividade 16.js

--- atividade 16.js
+++
@@ -1,7 +0,0 @@
-function verificarStringVazia(str) {
-    if (str.length === 0) {
-        console.log("A string está vazia.");
-    } else {
-        console.log("A string não está vazia.");
-    }
-}
