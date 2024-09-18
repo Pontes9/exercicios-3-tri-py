39276c1119ddf431f895f9d6480adae17cf5bf87
Pontes9 <162999567+Pontes9@users.noreply.github.com>
2024-09-18 11:32:38 -0300

    Delete atividade 15.js

--- atividade 15.js
+++
@@ -1,7 +0,0 @@
-function verificarMaiorIdade(idade) {
-    if (idade >= 18) {
-        console.log("A pessoa é maior de idade.");
-    } else {
-        console.log("A pessoa é menor de idade.");
-    }
-}
